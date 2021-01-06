c960b7910b353272383198da1864154f60b66d8c
Luka-Spa <spaninksluka@gmail.com>
2021-01-06 08:43:33 +0100

    -add url for websocket

--- Meet-a-stranger.React/src/components/App.js
+++ Meet-a-stranger.React/src/components/App.js
@@ -11,7 +11,7 @@ import { v4 as uuidv4 } from "uuid";
 
 export default function App() {
   const id = uuidv4();
-  const socket = io("http://localhost:3001");
+  const socket = io("https://meet-a-stranger-websocket.herokuapp.com/");
   socket.on("connect", () => onConnect());
 
   const onConnect = () => {
