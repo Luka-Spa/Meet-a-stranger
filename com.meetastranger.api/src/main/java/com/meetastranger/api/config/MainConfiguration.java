f83f711e31883381a8cc58a6c18ce822eed2a40a
Luka-Spa <spaninksluka@gmail.com>
2021-01-15 19:48:18 +0100

    change corse config

--- com.meetastranger.api/src/main/java/com/meetastranger/api/config/MainConfiguration.java
+++ com.meetastranger.api/src/main/java/com/meetastranger/api/config/MainConfiguration.java
@@ -11,7 +11,7 @@ public class MainConfiguration {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("*").allowedOrigins("*");
+				registry.addMapping("/**").allowedOrigins("*");
 			}
 		};
 	}
