cc2b1998148053bcb2a6c24be4bb2dc39ac9fffb
Luka-Spa <spaninksluka@gmail.com>
2021-01-15 18:26:17 +0100

    allow cors

--- com.meetastranger.api/src/main/java/com/meetastranger/api/config/MainConfiguration.java
+++ com.meetastranger.api/src/main/java/com/meetastranger/api/config/MainConfiguration.java
@@ -1,8 +1,18 @@
 package com.meetastranger.api.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MainConfiguration {
 
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("*").allowedOrigins("*");
+			}
+		};
+	}
 }
